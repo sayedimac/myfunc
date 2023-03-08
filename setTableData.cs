d6490050086ac553a5e8709267c1691f2398492f
Johan Myburgh <myburgh@gmail.com>
2023-03-08 08:36:29 +0000

    anonymous

--- setTableData.cs
+++ setTableData.cs
@@ -16,7 +16,7 @@ namespace myfunc
     {
         [FunctionName("setTableData")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
