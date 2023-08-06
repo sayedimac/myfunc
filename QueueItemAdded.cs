a080290dde35696c24798396d45131f35de17c68
sayedimac <myburgh@gmail.com>
2023-08-06 10:43:31 +0100

    queue added

---
+++ QueueItemAdded.cs
@@ -0,0 +1,25 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace myFunc
+{
+    public class QueueItemAdded
+    {
+        [FunctionName("QueueItemAdded")]
+        [return: Table("salesorders", Connection = "AzureWebJobsStorage")]
+        public void Run([QueueTrigger("salesorders", Connection = "AzureWebJobsStorage")]string myQueueItem, ILogger log)
+        {
+            log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+            return new MyPoco { PartitionKey = "salesorders", RowKey = Guid.NewGuid().ToString(), Text = myQueueItem };
+    }
+        }
+    }
+        public class MyPoco
+    {
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public string Text { get; set; }
+    }
+}
