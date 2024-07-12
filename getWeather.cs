4cc642fb150d1ecd32bd53ad1b47ae437341b459
Johan Myburgh <myburgh@gmail.com>
2024-07-12 21:03:16 +0100

    boggerop - weather added

--- getWeather.cs
+++ getWeather.cs
@@ -6,7 +6,7 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Microsoft.Text.Json;
+using System.Text.Json;
 
 namespace myfunc
 {
