c92ea7de6ac7d9efcc0b2d34212c4d02ff6644df
Johan Myburgh <myburgh@gmail.com>
2024-07-12 21:11:39 +0100

    Update getWeather.cs

--- getWeather.cs
+++ getWeather.cs
@@ -14,7 +14,7 @@ namespace myfunc
     {
         [FunctionName("getWeather")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("Sent Weather Data");
